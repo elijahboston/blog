c39fa3278b5f77ad30da77073d1c868768a1764e
Elijah Boston <e.b.boston@gmail.com>
2021-09-07 09:28:46 -0400

    change title

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 export const isClient = !(typeof window === 'undefined')
 export const isServer = typeof window === 'undefined'
 
-export const SITE_TITLE = 'the website of elijah boston'
+export const SITE_TITLE = 'elijah boston'
 export const POST_DIR = '_posts'
 export const CONTENT_DIR = '_content'
